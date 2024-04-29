5deb522931307ddc33d6afb4d29082fa3b5d7b5a
Renoy37 <robinmgk077@gmail.com>
2024-04-29 12:54:02 +0300

    Updated files

---
+++ __tests__/test.js
@@ -0,0 +1,25 @@
+const isBalancedB = require('../challenge2'); 
+
+
+describe('isBalancedB function', () => {
+  test('Balanced brackets: "[()]{}{[()()]()}"', () => {
+    expect(isBalancedB("[()]{}{[()()]()}")).toBe(true);
+  });
+
+  test('Balanced brackets: "([{}])"', () => {
+    expect(isBalancedB("([{}])")).toBe(true);
+  });
+
+
+  test('Unbalanced brackets: "{(})[]"', () => {
+    expect(isBalancedB("{(})[]")).toBe(false);
+  });
+
+  test('Unbalanced brackets: "[(])"', () => {
+    expect(isBalancedB("[(])")).toBe(false);
+  });
+
+  test('Unbalanced brackets: "[()"', () => {
+    expect(isBalancedB("[()")).toBe(false);
+  });
+});
